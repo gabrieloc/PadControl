d2292b4ff6ceae798719964a62b1bf413364a3a7
Gabriel O'Flaherty-Chan <hi@gabrieloc.com>
2023-04-21 10:51:22 -0400

    convert to SPM

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PadControl",
+    platforms: [
+        .iOS(.v11),
+        .macOS(.v10_13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "PadControl",
+            targets: ["PadControl"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "PadControl"),
+    ]
+)
